43c6820967b8b59dda04df564b25214cc51f1f9e
Aman120403 <amanmaurya904463@gmail.com>
2026-05-24 23:47:56 +0530

    commit for today

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
     const navigate = useNavigate();
     const [activeNav, setActiveNav] = useState('Home')
     const navLink = ['Home', 'About', 'Learn', 'ContactUs'];
+    const Mern = ["MongoDB", "ExpressJS", "ReactJS", "NodeJS"];
     const LoginNav = () =>{
         navigate("/login");
     }
@@ -41,131 +42,136 @@ const Home = () => {
                 <div >
                     <button 
                     onClick={LoginNav}
-                    className=" px-2 py-1 hover:underline hover:bg-blue-600 hover:text-white rounded-lg font-medium text-gray-400">
+                    className=" px-2 py-1 mr-1 hover:underline bg-blue-600 text-white rounded-lg font-medium text-gray-400">
                         Login
                     </button>
-                    <span className='text-gray-400 font-bold'>/</span>
+                    <span className='text-blue-600 font-bold'>/</span>
                     <button 
                     onClick={SignupNav}
-                    className=" px-2 py-1 hover:underline hover:bg-blue-600 hover:text-white rounded-lg font-medium text-gray-400">
+                    className=" px-2 py-1 ml-1 hover:underline bg-blue-600 text-white rounded-lg font-medium text-gray-400">
                         signUp
                     </button>
                 </div>
             </div>
+        </header>
+        
+
+        <main>
             <div className="bg-gradient-to-r from-blue-600/20 via-purple-600/10 to-pink-600/10 border-blue-500/20 flex justify-center items-center text-center p-3">
                 <p className=" text-gray-400 font-bold text-lg italic max-w-2xl">
                     “The time will pass no matter what you do, so choose to spend it learning things that make you better.”
                 </p>
             </div>
-        </header>
-        
+            <section className='bg-gradient-to-r from-blue-600/20 via-purple-600/10 to-pink-600/10 border-blue-500/20 p-3 border-b'>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 place-items-center">
 
-        <main className='bg-gradient-to-r from-blue-600/20 via-purple-600/10 to-pink-600/10 border-blue-500/20 p-3 border-b'>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 place-items-center">
+                    {/* MongoDB */}
+                    <div className="flex flex-col items-center gap-6 w-full">
+                        <img 
+                            className="w-64 h-64 sm:w-44 sm:h-44 mt-4 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]" 
+                            src={MongoPhoto} 
+                            alt="MongoDB pic"
+                        />
+                        <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
+                            <span className="text-purple-400">db</span>.
+                            <span className="text-yellow-300">users</span>.
+                            <span className="text-green-400">insertOne</span>
+                            {"({\n"}
+                            {"  "}<span className="text-orange-400">name</span>:{" "}
+                            <span className="text-green-300">"Mern"</span>,{"\n"}
+                            {"  "}<span className="text-orange-400">role</span>:{" "}
+                            <span className="text-green-300">"Developer"</span>{"\n"}
+                            {"  )\n"}
+                            {"}"}
+                        </pre>
+                    </div>
 
-                {/* MongoDB */}
-                <div className="flex flex-col items-center gap-6 w-full">
-                    <img 
-                        className="w-64 h-64 sm:w-44 sm:h-44 mt-4 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]" 
-                        src={MongoPhoto} 
-                        alt="MongoDB pic"
-                    />
-                    <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
-                        <span className="text-purple-400">db</span>.
-                        <span className="text-yellow-300">users</span>.
-                        <span className="text-green-400">insertOne</span>
-                        {"({\n"}
-                        {"  "}<span className="text-orange-400">name</span>:{" "}
-                        <span className="text-green-300">"Mern"</span>,{"\n"}
-                        {"  "}<span className="text-orange-400">role</span>:{" "}
-                        <span className="text-green-300">"Developer"</span>{"\n"}
-                        {"  )\n"}
-                        {"}"}
-                    </pre>
+                    {/* ExpressJS */}
+                    <div className="flex flex-col items-center gap-6 w-full">
+                        <img 
+                            className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
+                            src={ExpressPhoto} 
+                            alt="ExpressJs pic"
+                        />
+                        <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
+                            <span className="text-yellow-300">app</span>.
+                            <span className="text-cyan-400">get</span>
+                            {"("}
+                            <span className="text-green-300">"/"</span>, (req, res) =&gt; {"{\n"}
+                            {"  "}res.
+                            <span className="text-cyan-400">send</span>
+                            ({"\n"}
+                            {"    "}
+                            <span className="text-green-300">"Hello Express"</span>
+                            {"\n"}
+                            {"  )"}{"\n"}
+                            {"})"}
+                        </pre>
+                    </div>
+
+                    {/* React */}
+                    <div className="flex flex-col items-center gap-6 w-full">
+                        <img 
+                            className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
+                            src={ReactPhoto} 
+                            alt="React pic"
+                        />
+                        <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
+                            <span className="text-purple-400">function</span>{" "}
+                            <span className="text-yellow-300">App</span>()
+                            {" {\n"}
+                            {"  "}
+                            <span className="text-purple-400">return</span>{" ("}{"\n"}
+                            {"    "}
+                            <span className="text-blue-400">&lt;h1&gt;</span>
+                            Hello React
+                            <span className="text-blue-400">&lt;/h1&gt;</span>
+                            {"\n"}
+                            {"  )\n"}
+                            {"}"}
+                        </pre>
+                    </div>
+
+                    {/* NodeJS */}
+                    <div className="flex flex-col items-center gap-6 w-full">
+                        <img
+                            src={NodePhoto}
+                            alt="NodeJs pic"
+                            className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
+                        />
+                        <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
+                            <span className="text-purple-400">const</span>{" "}
+                            <span className="text-yellow-300">http </span>
+                            = require
+                            {"("}
+                            <span className="text-yellow-300">"http"</span>
+                            {")"}
+                            {"\n"}
+                            <span className="text-yellow-300">http</span>.
+                            <span className="text-cyan-400">createServer</span>
+                            {"("}{"\n"}
+                            {"(req, res) => {\n"}
+                            {"  "}res.
+                            <span className="text-cyan-400">end</span>
+                            {"("}
+                            <span className="text-green-300">"Hello Node"</span>
+                            {")"}
+                            {"\n"}
+                            {"})."}
+                            <span className="text-cyan-400">listen</span>
+                            {"(3000)"}
+                        </pre>
+                    </div>
+
                 </div>
+            </section>
+            <section>
 
-                {/* ExpressJS */}
-                <div className="flex flex-col items-center gap-6 w-full">
-                    <img 
-                        className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
-                        src={ExpressPhoto} 
-                        alt="ExpressJs pic"
-                    />
-                    <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
-                        <span className="text-yellow-300">app</span>.
-                        <span className="text-cyan-400">get</span>
-                        {"("}
-                        <span className="text-green-300">"/"</span>, (req, res) =&gt; {"{\n"}
-                        {"  "}res.
-                        <span className="text-cyan-400">send</span>
-                        ({"\n"}
-                        {"    "}
-                        <span className="text-green-300">"Hello Express"</span>
-                        {"\n"}
-                        {"  )"}{"\n"}
-                        {"})"}
-                    </pre>
-                </div>
-
-                {/* React */}
-                <div className="flex flex-col items-center gap-6 w-full">
-                    <img 
-                        className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
-                        src={ReactPhoto} 
-                        alt="React pic"
-                    />
-                    <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
-                        <span className="text-purple-400">function</span>{" "}
-                        <span className="text-yellow-300">App</span>()
-                        {" {\n"}
-                        {"  "}
-                        <span className="text-purple-400">return</span>{" ("}{"\n"}
-                        {"    "}
-                        <span className="text-blue-400">&lt;h1&gt;</span>
-                        Hello React
-                        <span className="text-blue-400">&lt;/h1&gt;</span>
-                        {"\n"}
-                        {"  )\n"}
-                        {"}"}
-                    </pre>
-                </div>
-
-                {/* NodeJS */}
-                <div className="flex flex-col items-center gap-6 w-full">
-                    <img
-                        src={NodePhoto}
-                        alt="NodeJs pic"
-                        className="w-64 h-64 sm:w-44 mt-4 sm:h-44 xl:w-64 xl:h-64 object-cover rounded-full ring-4 ring-blue-500 hover:ring-green-500 ring-offset-4 ring-offset-gray-900 cursor-pointer hover:scale-105 transition duration-200 hover:shadow-[0_0_40px_#22c55e]"
-                    />
-                    <pre className="bg-gray-900 text-green-400 p-6 rounded-xl text-left font-mono whitespace-pre-wrap text-lg w-full">
-                        <span className="text-purple-400">const</span>{" "}
-                        <span className="text-yellow-300">http </span>
-                        = require
-                        {"("}
-                        <span className="text-yellow-300">"http"</span>
-                        {")"}
-                        {"\n"}
-                        <span className="text-yellow-300">http</span>.
-                        <span className="text-cyan-400">createServer</span>
-                        {"("}{"\n"}
-                        {"(req, res) => {\n"}
-                        {"  "}res.
-                        <span className="text-cyan-400">end</span>
-                        {"("}
-                        <span className="text-green-300">"Hello Node"</span>
-                        {")"}
-                        {"\n"}
-                        {"})."}
-                        <span className="text-cyan-400">listen</span>
-                        {"(3000)"}
-                    </pre>
-                </div>
-
-            </div>
+            </section>
         </main>
-        <footer>
+        {/* <footer>
             <h4 className='text-white p-4'>Ab karo MERN stack interview ki taiyari</h4>
-        </footer>
+        </footer> */}
         
         
 
